f11f80b5cd58d8bcdcbef83811eb417cd0cae18d
rajagoah <rajagoah@mail.uc.edu>
2021-05-23 19:16:18 -0400

    added code for logging

--- multithreading.py
+++ multithreading.py
@@ -1,4 +1,6 @@
 import threading
+from time import time
+import logging
 
 def calc(num1,num2):
     return num1*num2
@@ -7,6 +9,11 @@ def calc_2(num1,num2):
     return num1-num2
 
 if __name__=="__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+
+    ts = time()
+    logger.info(" The session is starting %s:", ts)
     t1 = threading.Thread(target=calc, args=(3,2))
     t2 = threading.Thread(target=calc, args=(1, 2))
 
@@ -17,4 +24,5 @@ if __name__=="__main__":
     x = t1.join()
     y = t2.join()
     print(x,y)
-    print('success')
+
+    logger.info(" The session has ended %s:", time()-ts)
